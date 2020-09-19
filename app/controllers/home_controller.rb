5a5667a803a2ef35bc3b72fa197e247b1064b9b4
João Daniel <jotaf.daniel@gmail.com>
2020-09-19 17:55:39 -0300

    add HomeController#index
    Closes #24
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+
+  # por convenção, quando esta função for chamada,
+  # ela vai buscar por uma view específica:
+  # views/home/index.html.erb
+  def index
+  end
 end
