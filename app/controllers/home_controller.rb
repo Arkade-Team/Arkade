286da1e3cb1c8e8cabc170cca346fc218e2a0a31
João Daniel <jotaf.daniel@gmail.com>
2020-09-19 18:07:08 -0300

    link root to home#index
    change team members to be dynamic with erb
    
    Closes #23
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,14 @@ class HomeController < ApplicationController
   # ela vai buscar por uma view específica:
   # views/home/index.html.erb
   def index
+    @team = [
+      "Alécio Godoi",
+      "Daniel Quiteque",
+      "Fábio Nogueira",
+      "Gustavo Molina",
+      "João Daniel",
+      "Pedro França",
+      "Fulano de Tal"
+    ]
   end
 end
