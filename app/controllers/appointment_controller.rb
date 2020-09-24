33a3bf0928cc9181876c8f448685243f5c7e6e7d
João Daniel <jotaf.daniel@gmail.com>
2020-09-24 15:32:51 -0300

    create AppointmentController#create
    - 'rails generate controller Appointment create'
    
    Closes #27
    Closes #28
    Closes #29
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>
    Signed-off-by: Gustavo Molina <gustavo.liceu@gmail.com>
    Signed-off-by: Alécio Godoi <alecio.godoi@gmail.com>

---
+++ app/controllers/appointment_controller.rb
@@ -0,0 +1,18 @@
+class AppointmentController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+    @appointment = Appointment.new appointment_params
+
+    if @appointment.save
+      render json: @appointment
+    else
+      render json: { errors: @appointment.errors }
+    end
+  end
+
+  private
+    def appointment_params
+      params.require(:appointment).permit(:age, :sex)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  # GET "/"
+  post '/appointments', to: "appointment#create"
   root "home#index"
 end
