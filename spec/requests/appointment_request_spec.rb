de9abcb0525cf2aed290ac873eb7b435e973b947
João Daniel <jotaf.daniel@gmail.com>
2020-09-25 13:11:27 -0300

    FIX: remove useless test
    interface testing will be a subtask of acceptance testing
    with cucumber and user stories
    
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- spec/requests/appointment_request_spec.rb
+++ spec/requests/appointment_request_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe "Appointments", type: :request do
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/appointment/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
