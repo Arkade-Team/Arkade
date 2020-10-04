52b27366834f9775eafb22fdec67dee8004a863e
João Daniel <jotaf.daniel@gmail.com>
2020-10-04 12:20:59 -0300

    add tests to disease model
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- spec/models/disease_spec.rb
+++ spec/models/disease_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Disease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    Disease.delete_all
+    @disease = Disease.new
+  end
+
+  it "fails to create without name" do
+    expect(@disease).not_to be_valid
+  end
+
+  it "fails to create duplicated name" do
+    dup_name = "Foo"
+    Disease.create(name: dup_name)
+    @disease.name = dup_name
+    expect(@disease).not_to be_valid
+  end
+
+  it "fails to create names that are too short" do
+    @disease.name = "a"
+    expect(@disease).not_to be_valid
+  end
 end
