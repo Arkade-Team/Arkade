22725a9c055367ee1b1ae0856c267bddfc4a05a6
João Daniel <jotaf.daniel@gmail.com>
2020-10-06 20:57:53 -0300

    fix bootstrap installation
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "bootstrap";
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+import "../src/style.scss"
+require("jquery");
+require("bootstrap");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
