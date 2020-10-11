71eb12b9c53b1e5ea5cb0c9922138f69788a07cb
Alecio Godoi <alecio.godoi@ymail.com>
2020-10-11 15:20:15 -0300

    Criação Gráfico das últimas 15 diárias - Correção

--- features/step_definitions/exibicao_grafico_15_diarias.rb
+++ features/step_definitions/exibicao_grafico_15_diarias.rb
@@ -1,7 +1,7 @@
-Quando "o cliente acessa a página de consultas para acessar o gráfico de 15 diárias" do
+Quando "o cliente acessa a página de consultas para acessar o gráfico de quinze diárias" do
   visit '/appointments'
 end
 
-Então "ele vê um gráfico carregando informações das últimas 15 diárias por gênero" do
+Então "ele vê um gráfico carregando informações das últimas quinze diárias por gênero" do
     page.should have_content "Loading.."
 end
