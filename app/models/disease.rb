91ccda866b7ffc0993043addfc31d72aa0409992
João Daniel <jotaf.daniel@gmail.com>
2020-10-15 00:03:46 -0300

    refactor to count appointments with two diseases
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -6,6 +6,19 @@ class Disease < ApplicationRecord
     length: { minimum: 2 }
 
   def self.related_pairs
-    {"Osteoporose" => { "Depressão" => 1 }}
+    two_disease_apps = Appointment.all.filter do |app|
+      app.diseases.size == 2
+    end
+
+    hash = {}
+
+    two_disease_apps.each do |app|
+      diseases_name = app.diseases.map{ |d| d.name }.sort
+
+      hash[diseases_name[0]] ||= { diseases_name[1] => 0 }
+      hash[diseases_name[0]][diseases_name[1]] += 1
+    end
+
+    hash
   end
 end
