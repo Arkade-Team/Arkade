8443f74cd6ce4152d468c9442502cf3e48c2411a
pedr0hug0 <pedr0hug0@yahoo.com.br>
2020-10-20 18:27:05 -0300

    Try to fix Cognitive Complexity

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -9,11 +9,7 @@ class AppointmentController < ApplicationController
     
     ###Consultas por período###
     @lastFifteenDaysPeriodo = Appointment.where("created_at >= ?", @fifteenDaysAgo ).group('date(created_at), id').order('date(created_at)')
-    #Adicionar 1h@consulta = Time.current + (1*3600);
-    #@consulta = Time.current + (7*3600);
-    #iniciando variaves
-    @periodo = "indefinido"
-    @periodo_antigo = "indefinido"
+       
     @madrugada = 0
     @total_madrugada = 0
     @manha = 0
@@ -25,66 +21,51 @@ class AppointmentController < ApplicationController
     @total_itens = 0
     @consulta_antiga = Time.current + 3600; #ConsultaAntiga incia no futuro.
     @arrayPeriodo = []
-         
-     
+    
          @lastFifteenDaysPeriodo.each do |item|         
-         @total_itens += 1
-         #adicionando 1h para cada consulta efetuada (randomizar horarios consultas)
-         @consulta = item.created_at + (@total_itens * 3600);  
-        
-         #enquanto data igual, faz regraPeriodo()
-         if @consulta.to_date == @consulta_antiga.to_date
-         regraPeriodo() #setando periodo e total periodo
-         
-        else
-        @madrugada = 0
-        @manha = 0
-        @tarde = 0
-        @noite = 0
-        @consulta_antiga = @consulta.to_date
-        regraPeriodo() #setando periodo e total periodo
-        end
-        
-        if @arrayPeriodo.include?(@consulta.strftime("%d/%m/%Y")) == false
-        @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite
-        
-        else
-        
-        @arrayPeriodo.pop(5)
-        @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite
-        
-        end
-          
-        
+            @total_itens += 1
+            #adicionando 1h
+            @consulta = item.created_at + (@total_itens * 3600);  
+            if @consulta.to_date == @consulta_antiga.to_date
+              regraPeriodo() #setando periodo e total periodo
+            else
+              @madrugada = 0
+              @manha = 0
+              @tarde = 0
+              @noite = 0
+              @consulta_antiga = @consulta.to_date
+              regraPeriodo() #setando periodo e total periodo
+            end
+            
+            if @arrayPeriodo.include?(@consulta.strftime("%d/%m/%Y")) == false
+              @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite
+            else
+              @arrayPeriodo.pop(5)
+              @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite            
+            end
         end #end item
   end #index
 
   def regraPeriodo
     #Regra divisão período
     if @consulta.hour >=0 && @consulta.hour < 6
-    @periodo = "Madrugada"
-    @total_madrugada +=1
-    @madrugada +=1 
+      @periodo = "Madrugada"
+      @total_madrugada +=1
+      @madrugada +=1 
     elsif  @consulta.hour >=6 && @consulta.hour < 12
-    @periodo = "Manhã" 
-    @total_manha +=1
-    @manha +=1 
+      @periodo = "Manhã" 
+      @total_manha +=1
+      @manha +=1 
     elsif  @consulta.hour >=12 && @consulta.hour < 18
-    @periodo = "Tarde"
-    @total_tarde +=1 
-    @tarde +=1
-    elsif  @consulta.hour >=18 && @consulta.hour <= 23
-    @periodo = "Noite" 
-    @total_noite +=1
-    @noite +=1
-    else 
-    @periodo = "Indefinido"
-    @contador = 0
+      @periodo = "Tarde"
+      @total_tarde +=1 
+      @tarde +=1
+    else  @consulta.hour >=18 && @consulta.hour <= 23
+      @periodo = "Noite" 
+      @total_noite +=1
+      @noite +=1
     end
   end
-  
-         
-
   
   def create
     @appointment = Appointment.new appointment_params
