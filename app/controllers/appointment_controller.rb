6825d3fbbbe195efaf5d029846db71cab4b7a7f9
pedr0hug0 <pedr0hug0@yahoo.com.br>
2020-10-21 15:01:40 -0300

    Fix Cognitive Complexity

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -7,7 +7,6 @@ class AppointmentController < ApplicationController
     @fifteenDaysAgo = Time.now - (@daysAgo * 24 * 60 * 60)
     @lastFifteenDays = Appointment.where("created_at >= ?", @fifteenDaysAgo ).group('sex').order('sex').group('date(created_at)').order('date(created_at)').count
     
-    ###Consultas por período###
     @lastFifteenDaysPeriodo = Appointment.where("created_at >= ?", @fifteenDaysAgo ).group('date(created_at), id').order('date(created_at)')
        
     @madrugada = 0
@@ -21,33 +20,10 @@ class AppointmentController < ApplicationController
     @total_itens = 0
     @consulta_antiga = Time.current + 3600; #ConsultaAntiga incia no futuro.
     @arrayPeriodo = []
-    
-         @lastFifteenDaysPeriodo.each do |item|         
-            @total_itens += 1
-            #adicionando 1h
-            @consulta = item.created_at + (@total_itens * 3600);  
-            if @consulta.to_date == @consulta_antiga.to_date
-              regraPeriodo() #setando periodo e total periodo
-            else
-              @madrugada = 0
-              @manha = 0
-              @tarde = 0
-              @noite = 0
-              @consulta_antiga = @consulta.to_date
-              regraPeriodo() #setando periodo e total periodo
-            end
-            
-            if @arrayPeriodo.include?(@consulta.strftime("%d/%m/%Y")) == false
-              @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite
-            else
-              @arrayPeriodo.pop(5)
-              @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite            
-            end
-        end #end item
-  end #index
+    PreencherArrayPeriodo()     
+  end
 
   def regraPeriodo
-    #Regra divisão período
     if @consulta.hour >=0 && @consulta.hour < 6
       @periodo = "Madrugada"
       @total_madrugada +=1
@@ -64,6 +40,30 @@ class AppointmentController < ApplicationController
       @periodo = "Noite" 
       @total_noite +=1
       @noite +=1
+    end
+  end
+
+  def PreencherArrayPeriodo
+    @lastFifteenDaysPeriodo.each do |item|         
+      @total_itens += 1
+      @consulta = item.created_at + (@total_itens * 3600);  
+      if @consulta.to_date == @consulta_antiga.to_date
+        regraPeriodo()
+      else
+        @madrugada = 0
+        @manha = 0
+        @tarde = 0
+        @noite = 0
+        @consulta_antiga = @consulta.to_date
+        regraPeriodo()
+      end
+      
+      if @arrayPeriodo.include?(@consulta.strftime("%d/%m/%Y")) == false
+        @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite
+      else
+        @arrayPeriodo.pop(5)
+        @arrayPeriodo << @consulta.strftime("%d/%m/%Y") << @madrugada << @manha << @tarde << @noite            
+      end
     end
   end
   
