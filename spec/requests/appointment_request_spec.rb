9806382bd62879317290176d4e2fcf5bd5a0037b
Gustavo Molina <gustavo.liceu@gmail.com>
2020-10-21 17:28:17 -0300

    Correção no arquivo appointment_request_spec.rb

--- spec/requests/appointment_request_spec.rb
+++ spec/requests/appointment_request_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "Appointments", type: :request do
-    it 'calls appointment/index template view' do
-        get '/appointment'
-        expect(response).to render_template('index')
-      end
-
+    
 end
