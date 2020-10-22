5c753ae7a86445752ea4917ee19afec4b535114b
Gustavo Molina <gustavo.liceu@gmail.com>
2020-10-22 12:18:11 -0300

    Revert "Nova Correção da feature exibição_gráfico_distribuição_sexo"
    This reverts commit add7e03921da28bacc347d1bc197645ae84bd404.

--- features/step_definitions/exibicao_grafico_distribuicao_sexo.rb
+++
@@ -1,7 +0,0 @@
-Quando "o cliente acessa a página de consultas para acessar o gráfico de distribuição por sexo" do
-  visit '/appointments'
-end
-
-Então "ele vê um gráfico carregando informações sobre o sexo dos pacientes" do
-    page.should have_content "Loading.."
-end
