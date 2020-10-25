e5889b944b262d5c37099e3755172ae76f2326dc
Fábio Nogueira de Almeida <fna.contact@gmail.com>
2020-10-25 19:32:48 -0300

    add tests

---
+++ features/step_definitions/criacao_acao_step.rb
@@ -0,0 +1,10 @@
+Quando "o coordenador acessar a página de ações" do
+    visit '/actions'
+end
+
+Então "deverá ver um formulário contendo as ações cadastradas" do
+    page.should have_content "Nome"
+    page.should have_content "Resultado"
+    page.should have_content "Qt. Consultas"
+end
+  
