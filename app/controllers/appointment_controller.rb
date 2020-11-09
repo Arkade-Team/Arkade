14273e3e39e6940c0f7c964fb345cf6f4f30c6f4
pedr0hug0 <pedr0hug0@yahoo.com.br>
2020-11-09 10:42:34 -0300

    Adicionado botao detalhes consultas. Botao de exportar cada grafico. Melhorado layout e dados padrao de parametros de filtro de dias

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -3,11 +3,11 @@ class AppointmentController < ApplicationController
 
   def index 
     @appointments = Appointment.all
-    @daysAgo = params[:days].present? ? params[:days].to_i.abs : 0
+    @daysAgo = params[:days].present? ? params[:days].to_i.abs : 15
     @fifteenDaysAgo = @daysAgo != 0? (Time.now - (@daysAgo * 24 * 60 * 60)) : (Date.today - (@daysAgo * 24 * 60 * 60))
     @lastFifteenDays = Appointment.where("created_at >= ? and created_at <= ?", @fifteenDaysAgo.beginning_of_day, Time.now).group('sex').order('sex').group('date(created_at)').order('date(created_at)').count
     @sex = Appointment.group('sex').order('sex').count
-    @lastFifteenDaysPeriodo = Appointment.where("created_at >= ? and created_at <= ?", @fifteenDaysAgo, Time.now).order('date(created_at)')
+    @lastFifteenDaysPeriodo = Appointment.where("created_at >= ? and created_at <= ?", @fifteenDaysAgo.beginning_of_day, Time.now).order('date(created_at)')
 
     @diseasesByAge = Appointment.joins(:diseases).group('diseases.name').group('appointments.age').count
     @sexByDiseases = Appointment.joins(:diseases).group('appointments.sex').group('diseases.name').count
--- features/step_definitions/exibicao_consultas_por_periodo.rb
+++ features/step_definitions/exibicao_consultas_por_periodo.rb
@@ -2,7 +2,11 @@ Quando('o coordenador acessa a página de consultas por período') do
     visit '/appointments'
   end
   
-  Então('ele vê uma tabela contendo a quantidade de consultas por período') do
+  Então('ele vê uma tabela contendo o total de consultas por período') do
     page.should have_content "Período"
-    
+  end
+  
+  Então('um botão {string} para o detalhamento diario.') do |string|
+    page.should have_content "Detalhes"
+    page.should have_selector("div.detalhes_periodo")
   end
