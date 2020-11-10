2e7c92aacb24e44d92ce2751d96d834a220df4a6
Alecio Godoi <alecio.godoi@ymail.com>
2020-11-09 21:53:37 -0300

    Cadastro dos Médicos - 09-11-2020

---
+++ db/migrate/20201109191058_create_doctors.rb
@@ -0,0 +1,11 @@
+class CreateDoctors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :doctors do |t|
+      t.string :crm
+      t.string :name
+      t.string :specialty
+
+      t.timestamps
+    end
+  end
+end
